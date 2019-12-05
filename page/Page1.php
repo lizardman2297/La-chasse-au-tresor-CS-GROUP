54672cb85bdafea7eafbf4711cf99fd78d864b9c
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-05 20:02:21 +0100

    tutoriel et css

--- page/Page1.php
+++ page/Page1.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="./css/page.css" rel="stylesheet">
+        <link type="text/css" href="./css/page.css" rel="stylesheet">
     </head>
     <body>
         <h1>Page1</h1>
         <div class="corePage1">
             
-            <imput type=butom name="tuto1"><a href="./page/tutoriel/tuto1.php">tuto1</a></imput>
-            <imput type=butom name="tuto2"><a href="./page/tutoriel/tuto2.php">tuto2</a></imput>
-            <imput type=butom name="tuto3"><a href="./page/tutoriel/tuto3.php">tuto3</a></imput>
+            <imput type=butom name="tuto1"><a href="./page/tutoriel/tutoriel1.php">tutoriel 1</a></imput>
+            <imput type=butom name="tuto2"><a href="./page/tutoriel/tutoriel2.php">tutoriel 2</a></imput>
+            <imput type=butom name="tuto3"><a href="./page/tutoriel/tutoriel3.php">tutoriel 3</a></imput>
         </div>
 
     </body>
