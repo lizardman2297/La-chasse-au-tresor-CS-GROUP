e8ae58408d6de96c4b281ef7d6f5786d8fcedaad
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-06 00:24:53 +0100

    Update page4.php

--- page/page4.php
+++ page/page4.php
@@ -6,8 +6,59 @@
     </head>
     <body>
         <form>
+            <p>
+                Question 1 :
+                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                <select>
+                    <option value="Reponce"></option>
+                    <option value="Caf">Caf</option>
+                    <option value="Crous">Crous</option>
+                </select>
+            </p>
+            <p>
+                Question 2 :
+                Quelle est le montent maximal mensuel accorder par la bourse en 2019 ???
+                <select>
+                    <option value="Reponce"></option>
+                    <option value="561.20">561,20</option>
+                    <option value="483.10">483,10</option>
+                    <option value="3967">3967</option>
+                </select>
+            </p>
+            <p>
+                Question 3 :
+                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                <select>
+                    <option value="Reponce"></option>
+                    <option value="Caf">Caf</option>
+                    <option value="Crous">Crous</option>
+                </select>
+            </p>
+            <p>
+                Question 4 :
+                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                <select>
+                    <option value="Reponce"></option>
+                    <option value="Caf">Caf</option>
+                    <option value="Crous">Crous</option>
+                </select>
+            </p>
+            <p>
+                Question 5 :
+                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                <select>
+                    <option value="Reponce"></option>
+                    <option value="Caf">Caf</option>
+                    <option value="Crous">Crous</option>
+                </select>
+            </p>
 
-            <input type="text" name="Nom"  size="20">
+            <?php>
+                if(){
+                    echo"<input type='text' name='reponce' placeholder='Réponce' size='20'>";
+                    echo"<input type='submit' value='valider' href='.php'>";
+                }
+            </?>
 
         </form>
     </body>
