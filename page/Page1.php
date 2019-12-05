286234175019c0cbe20cccf858514f368b7cd264
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-05 21:34:23 +0100

    comit

--- page/Page1.php
+++ page/Page1.php
@@ -8,14 +8,17 @@
         <h1>Page1</h1>
         <div class="corePage1">
             
-            <div>
-                <a href="./tutoriel/tutoriel1.php">tutoriel 1<img src="../image/vrai-faux.png" alt="vrai-faux"></a>
+            <div >
+                <a href="./tutoriel/tutoriel1.php"><img src="../image/vrai-faux.png" alt="vrai-faux"></a>
+                <p class="tuto1">tutoriel 1</p>
             </div>
             <div>
-                <a href="./tutoriel/tutoriel2.php">tutoriel 2<img src="../image/numero.png" alt="vrai-faux"></a>
+                <a href="./tutoriel/tutoriel2.php"><img src="../image/numero.png" alt="numero"></a>
+                <p class="tuto2">tutoriel 2</p>
             </div>
             <div>
-                <a href="./tutoriel/tutoriel3.php">tutoriel 3</a>
+                <a href="./tutoriel/tutoriel3.php"><img src="../image/qr-code.png" alt="qr-code"></a>
+                <p class="tuto2">tutoriel 3</p>
             </div>
         </div>
 
