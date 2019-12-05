0c0a578ec38df530f36a93aeaadffbd94f339542
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-06 00:51:30 +0100

    Update page4.php

--- page/page4.php
+++ page/page4.php
@@ -12,7 +12,7 @@
                 <select>
                     <option value="Reponce"></option>
                     <option value="Caf">Caf</option>
-                    <option value="Crous">Crous</option>
+                    <option ! value="Crous">Crous</option>
                 </select>
             </p>
             <p>
@@ -20,43 +20,48 @@
                 Quelle est le montent maximal mensuel accorder par la bourse en 2019 ???
                 <select>
                     <option value="Reponce"></option>
-                    <option value="561.20">561,20</option>
+                    <option ! value="561.20">561,20</option>
                     <option value="483.10">483,10</option>
                     <option value="3967">3967</option>
                 </select>
             </p>
             <p>
                 Question 3 :
-                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                Quelle combient de crous sont disponible en france ???
                 <select>
                     <option value="Reponce"></option>
-                    <option value="Caf">Caf</option>
-                    <option value="Crous">Crous</option>
+                    <option ! value="28">28</option>
+                    <option value="96">96</option>
+                    <option value="159">159</option>
                 </select>
             </p>
             <p>
                 Question 4 :
-                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                Quelle combient y a t-il d'etudient bourcier en france en  ???
                 <select>
                     <option value="Reponce"></option>
-                    <option value="Caf">Caf</option>
-                    <option value="Crous">Crous</option>
+                    <option value="141 962">141 962</option>
+                    <option ! value="691">691 200</option>
+                    <option value="700">700 000</option>
+                    <option value="53">53 620</option>
                 </select>
             </p>
             <p>
                 Question 5 :
-                Quelle est la principal agence d'aide financiaire pour les etudient ???
+                Quel est le taux d'etudient francais eyent optenue une licence ou equivalant ???
                 <select>
                     <option value="Reponce"></option>
-                    <option value="Caf">Caf</option>
-                    <option value="Crous">Crous</option>
+                    <option ! value="Caf">43 %</option>
+                    <option value="Crous">34 %</option>
+                    <option value="Crous">86 %</option>
+                    <option value="Crous">4,7 %</option>
                 </select>
             </p>
 
             <?php>
                 if(){
-                    echo"<input type='text' name='reponce' placeholder='Réponce' size='20'>";
-                    echo"<input type='submit' value='valider' href='.php'>";
+                    echo "<input type='text' name='reponce' placeholder='Réponce' size='20'>";
+                    echo "<input type='submit' value='valider' href='.php'>";
                 }
             </?>
 
