3bafd47f4e886fb1cd51094db409f61853fb1298
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-05 20:32:31 +0100

    image

--- page/Page1.php
+++ page/Page1.php
@@ -2,12 +2,13 @@
 <html>
     <head>
         <link type="text/css" href="./css/page.css" rel="stylesheet">
+
     </head>
     <body>
         <h1>Page1</h1>
         <div class="corePage1">
             
-            <imput type=butom name="tuto1"><a href="./tutoriel/tutoriel1.php">tutoriel 1</a></imput>
+            <imput type=buttom name="tuto1"><img rsc="./image/vrai-faux.png"/><a href="./tutoriel/tutoriel1.php">tutoriel 1</a></imput>
             <imput type=butom name="tuto2"><a href="./tutoriel/tutoriel2.php">tutoriel 2</a></imput>
             <imput type=butom name="tuto3"><a href="./tutoriel/tutoriel3.php">tutoriel 3</a></imput>
         </div>
