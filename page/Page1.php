52d6cb22df07f3d9a7dd0d2e397ee202b17c601a
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-12-05 20:13:28 +0100

    Update Page1.php

--- page/Page1.php
+++ page/Page1.php
@@ -7,9 +7,9 @@
         <h1>Page1</h1>
         <div class="corePage1">
             
-            <imput type=butom name="tuto1"><a href="./page/tutoriel/tutoriel1.php">tutoriel 1</a></imput>
-            <imput type=butom name="tuto2"><a href="./page/tutoriel/tutoriel2.php">tutoriel 2</a></imput>
-            <imput type=butom name="tuto3"><a href="./page/tutoriel/tutoriel3.php">tutoriel 3</a></imput>
+            <imput type=butom name="tuto1"><a href="./tutoriel/tutoriel1.php">tutoriel 1</a></imput>
+            <imput type=butom name="tuto2"><a href="./tutoriel/tutoriel2.php">tutoriel 2</a></imput>
+            <imput type=butom name="tuto3"><a href="./tutoriel/tutoriel3.php">tutoriel 3</a></imput>
         </div>
 
     </body>
