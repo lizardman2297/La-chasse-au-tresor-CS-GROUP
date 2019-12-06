603d454948a6e0cda366a755cd98e81f642a6788
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2019-12-06 06:29:47 +0100

    Update redir-254698.php

--- methode/redir-254698.php
+++ methode/redir-254698.php
@@ -1,7 +1,7 @@
 <?php
 if ($_POST["code"] == "APL") {
-    require_once("db.php");
-    valadateTuto(1);
+    // require_once("db.php");
+    // valadateTuto(1);
     header("location: ../page/redir-254698.php?stat=1");
 }else {
     header("location: ../page/redir-254698.php?stat=0");
